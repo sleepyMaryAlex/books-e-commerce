9975855f95e310163dd87a7950ca15708022415a
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-04-26 18:01:04 +0200

    feat: add router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
-<script setup></script>
+<template>
+  <div class="wrapper">
+    <RouterView />
+  </div>
+</template>
 
-<template></template>
-
-<style scoped></style>
+<style lang="scss" scoped>
+.wrapper {
+  min-height: 100vh;
+  max-width: 1440px;
+  margin: 0 auto;
+  position: relative;
+}
+</style>
