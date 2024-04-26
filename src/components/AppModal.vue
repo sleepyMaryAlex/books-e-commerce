3a33a72decb4b721e82a6df0859117bb2c9463b8
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-04-26 17:53:34 +0200

    feat: add modal component

---
+++ src/components/AppModal.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { defineProps, defineEmits, ref, inject } from 'vue';
+import { onClickOutside } from '@vueuse/core';
+
+const { isAuthModalOpened, setAuthModalOpened } = inject('isAuthModalOpened');
+defineProps(['isDetailsModalOpened']);
+const emit = defineEmits(['closeDetailsModalOpened']);
+const modalContainer = ref(null);
+
+onClickOutside(modalContainer, () => {
+  setAuthModalOpened(false);
+  emit('closeDetailsModalOpened');
+});
+</script>
+
+<template>
+  <div v-if="isAuthModalOpened || isDetailsModalOpened" class="modal">
+    <div class="modal__container" ref="modalContainer">
+      <div class="modal__header">
+        <slot name="header"></slot>
+      </div>
+      <div class="modal__body">
+        <slot name="content"></slot>
+      </div>
+      <div class="modal__footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.modal {
+  position: fixed;
+  z-index: 9999;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  @include flex(column, center, center);
+}
+
+.modal__container {
+  min-height: 690px;
+  padding: 40px 56px;
+  background-color: $bg-light;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  @include flex(column, space-between, stretch);
+  gap: 40px;
+}
+
+.modal__footer {
+  @include flex(row, center, stretch);
+}
+</style>
