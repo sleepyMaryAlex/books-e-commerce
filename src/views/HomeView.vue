a7e60869360f84eeffeb646361f831976a238d47
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-04-29 11:11:29 +0200

    feat: add home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import AppHero from '../components/AppHero.vue';
+import AppHeader from '../components/AppHeader.vue';
+import AppProducts from '../components/AppProducts.vue';
+import { ref, provide } from 'vue';
+
+const isAuthModalOpened = ref(false);
+
+function setAuthModalOpened(isOpen) {
+  isAuthModalOpened.value = isOpen;
+}
+
+provide('isAuthModalOpened', { isAuthModalOpened, setAuthModalOpened });
+</script>
+
+<template>
+  <AppHeader />
+  <main>
+    <AppHero />
+    <AppProducts />
+  </main>
+</template>
